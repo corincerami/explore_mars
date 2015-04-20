70d92906515b4c1ffa47f94d78ef8d57f6431fd0
Chris C Cerami <chrisccerami@gmail.com>
2015-04-20 03:22:53 -0400

    Add earth_date method to Photo class

--- lib/explore_mars/photo.rb
+++ lib/explore_mars/photo.rb
@@ -2,6 +2,9 @@ module ExploreMars
   class Photo
     attr_reader :src, :sol, :camera
 
+    LANDING_DATE = DateTime.new(2012,8,6,5,17,57)
+    SOL_IN_SECONDS = 88775.244
+
     def initialize(src, sol, camera)
       @src = src
       @sol = sol
@@ -10,6 +13,11 @@ module ExploreMars
 
     def to_s
       @src
+    end
+
+    def earth_date
+      date = LANDING_DATE + (@sol.to_i * SOL_IN_SECONDS).seconds
+      date.strftime("%b %e, %Y")
     end
   end
 end
