d4e87dc17a9413dfdb9f3cbb301f268805aa70bb
Chris C Cerami <chrisccerami@gmail.com>
2015-06-13 16:33:37 -0400

    Fix test

--- spec/call_spec.rb
+++ spec/call_spec.rb
@@ -3,6 +3,6 @@ require "spec_helper"
 describe ExploreMars::Call do
   it "should have proper constants" do
     expect(ExploreMars::Call::BASE_URI).to eq "https://mars-curiosity-api.herokuapp.com/api/v1/rovers/"
-    expect(ExploreMars::Call::CAMERAS).to eq ["FHAZ", "RHAZ", "MAST", "CHEMCAM", "NAVCAM", "MAHLI", "MARDI"]
+    expect(ExploreMars::Call::CAMERAS).to eq ["FHAZ", "RHAZ", "MAST", "CHEMCAM", "NAVCAM", "MAHLI", "MARDI", "PANCAM", "MINITES"]
   end
 end
