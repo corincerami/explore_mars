8496bda940abde989ad847cd96b5ed75260321e0
Chris C Cerami <chrisccerami@gmail.com>
2015-07-18 00:41:55 -0400

    Update BASE_URI in spec

--- spec/call_spec.rb
+++ spec/call_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe ExploreMars::Call do
   it "should have proper constants" do
-    expect(ExploreMars::Call::BASE_URI).to eq "https://mars-curiosity-api.herokuapp.com/api/v1/rovers/"
+    expect(ExploreMars::Call::BASE_URI).to eq "https://mars-photos.herokuapp.com/api/v1/rovers/"
     expect(ExploreMars::Call::CAMERAS).to eq ["FHAZ", "RHAZ", "MAST", "CHEMCAM", "NAVCAM", "MAHLI", "MARDI", "PANCAM", "MINITES"]
   end
 end
